a52ea9e2790abae9ac5966b925caa3ced5421768
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-14 10:25:47 -0500

    created resume servlet

--- src/main/java/ResumeServlet.java
+++ src/main/java/ResumeServlet.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class ResumeServlet {
+@WebServlet(name = "ResumeServlet", urlPatterns = "/resume")
+public class ResumeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String content = "<h1>My Resume</h1>";
+        content += "<p>I write a lot of code. Check my GitHub</p>";
+        content += "<hr><br>";
+        content += "<a href='https://github.com/'>My GitHub Profile</a><br>";
+        content += "<a href='/portfolio'>My Portfolio</a>";
+
+        out.println(content);
+    }
+
+
 }
