c60c2f693f963d7c9cdf4e60e93bc86e8f825d08
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-14 13:41:30 -0500

    created portfolio servlet

--- src/main/java/PortfolioServlet.java
+++ src/main/java/PortfolioServlet.java
@@ -1,2 +1,22 @@
-public class PortfolioServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "PortfolioServlet", urlPatterns = "/portfolio")
+public class PortfolioServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String content = "<h1>My Portfolio</h1>";
+        content += "<p>my awesome portfolio</p> <br><hr><br>";
+        content += "<a href='/resume'>My Resume</a>";
+        
+        out.println(content);
+    }
 }
