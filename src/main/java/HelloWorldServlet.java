68d8c4b33bc8b28f6667fa243474372523c259ff
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-14 13:41:48 -0500

    made changes

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,24 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name"); //this is a parameter that is looking for the key word joke - this is passed in with the request. in the web browser that pops up.  add: /hello to see hello world.  add /hello?name=Bobbie to see hello bobbie;
+
+        if (name == null){//default response to nothing.
+            name = "World";
+        }
+        out.println("<h1> Hello " + name + "! </h1>");
 
     }
 }
