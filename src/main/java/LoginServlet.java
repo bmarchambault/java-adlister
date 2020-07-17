6deca9097b18c265c89d4a5dcbf8810b95bf6b4f
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-17 15:21:57 -0500

    created login servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -11,15 +11,25 @@ import java.io.PrintWriter;
 
 public class LoginServlet extends HttpServlet {
 
+//    a get request just grabs the info it needs and loads a page with that data.
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            req.getRequestDispatcher("/login.jsp").forward(req, resp);
-                if(req.getMethod().equalsIgnoreCase("post")) {
+//        this is telling the get which page to load.
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+//    it get values from froms, the servlet utilizes the do post method. the parameter name and the name on the form must match.   Basically here, the do post is reading information, applying it to the code and redirecting.
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     String username = req.getParameter("username");
                     String password = req.getParameter("password");
-                    if (username.equalsIgnoreCase("admin") && password.equals("password")) {
-                        resp.sendRedirect("/profile.jsp");
-                    }
+                    boolean validLogin = username.equalsIgnoreCase("admin") && password.equals("password");
+                     if (validLogin){
+//                         this is a get request and references a servlet
+                         resp.sendRedirect("/profile");
+                     }else{
+                         resp.sendRedirect("/login");
+                     }
                 }
-    }
+
 }
