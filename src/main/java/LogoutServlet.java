3e4fc2dbd7c7c349c86450b5bb9ceef386ab5dff
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-20 10:30:56 -0500

    fixed code errors, added logout button

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,15 +6,32 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-    @WebServlet(name = "LogoutServlet", urlPatterns = "/login")
-    public class LogoutServlet extends HttpServlet {
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
-        }
 
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-            HttpSession session = request.getSession();
-                session.removeAttribute("isAdmin");
-                response.sendRedirect("/login");
-            }
+
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        request.getSession().removeAttribute("user");
+//        request.getSession().invalidate();
+//        response.sendRedirect("/login");
+//    }
+//}
+////
+//    @WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+//    public class LogoutServlet extends HttpServlet {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.getRequestDispatcher("/WEB-INF/logout.jsp").forward(request, response);
         }
+//
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            request.getSession().removeAttribute("user");
+            request.getSession().invalidate();
+            response.sendRedirect("/login");
+        }
+}
+//            HttpSession session = request.getSession();
+//                session.removeAttribute("isAdmin");
+//                response.sendRedirect("/login");
+//            }
+//        }
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,10 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
@@ -19,3 +23,4 @@ public class ViewProfileServlet extends HttpServlet {
 //            session.setAttribute("background-color","blue");
 //        }
 //        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
